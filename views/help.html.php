07d50160e55f14341478ccd0f9f77186646fa278
pimteam <admin@pimteam.net>
2021-11-29 16:36:38 +0200

    working on help

--- views/help.html.php
+++ views/help.html.php
@@ -3,7 +3,9 @@
 <p>Shortcode Revolution is a tool that allows you to easily and reliable create various elements on your site. Dynamic elements with data, static design elements like buttons and tables, and so on.
 It's techically possible to create the shortcodes manually but it's much easier to use the shortcode generator from your Shortcode Revolution menu.</p>
 
-<h2>Post and Comments Shortcodes</h2>
+<h2>Available Shortcodes</h2>
+
+<h3>Post and Comments Shortcodes</h3>
 
 <p>This section allows you to generate shortcodes that dynamically list posts or comments based on search criteria. You can use the shortcodes in widgets or in your custom theme pages.</p>
 
@@ -19,12 +21,12 @@ It's techically possible to create the shortcodes manually but it's much easier 
 
 <p>The <b>comments</b> shortcode displays comments related to a post or just most recent / best comments. The display formats are default and carousel / slider.</p>
 
-<h2>Popups / Modals</h2>
+<h3>Popups / Modals</h3>
 
 <p>This shortcode lets you create modal windows / lightboxes with any predefined content. It generates a button to click on which appears on the place of the shortcode so you can use it anywhere in your site. This makes it super easy to create lightboxes without writing code or using more plugins.</p>
 <p>The modals can contain any kind of images and media, text, other shortcodes, and so on. If your modal contains anything other than plain unformtatted text make sure to insert it in Text mode of your rich text editor (when pasting it in posts, pages, etc).</p>
 
-<h2>Columns &amp; Grids</h2>
+<h3>Columns &amp; Grids</h3>
 
 <p>These shortcodes let you create modern cross-browser compatible and responsive content that flows in columns or a grid.</p>
 
@@ -32,14 +34,30 @@ It's techically possible to create the shortcodes manually but it's much easier 
 
 <p>The <b>grid option</b> is more precise in the way that your content flows on the page. You choose exactly what goes into each cell of the grid - the content does not low through the cells / columns. The grid option can also use your custom template.</p> 
 
-<h2>Tabs</h2>
+<h3>Tabs</h3>
 
 <p>The tabs shortcode lets you create tabbed content where the tabs do not reload the whole page. Very easy, light and responsive.</p>
 
-<h2>Buttons</h2>
+<h3>Buttons</h3>
 
 <p>With this shortcode you can create your own design for buttons - colors, size, style, fonts, and so on. Currently buttons support only URL action (going to another page). Calling custom JS functions is coming soon.</p>
 
-<h2>Tables</h2>
+<h3>Tables</h3>
 
 <p>The tables shortcode gives you a super easy way to turn an Excel (CSV) sheet into a HTML table. The data is loaded on the fly so you don't need to recreate the shortcode when the file contents change.</p>
+
+<h3>Flashcards</h3>
+
+<p>This shortcode creates nice interactive flashcards. Mostly used in education and educational games, flashcards typically ask a question and reveal the answer when clicked on them. The flashcards in Shortcode Revolution are just visual elements and do not (yet!) fire any back-end action when flipped.</p>
+
+<h3>Data Shortcodes</h3>
+
+<p>The data shortcodes are used to dynamically pull data from your WordPress database.</p>
+
+<p>We currently support <b>user data</b> shortcode. This shortcode allows you pull data for the currently logged in user, a predefined user, or an user defined by an URL parameter. The shortcode can return data for a specific field of the user profile or use meta, or return the whole user data in a string with variables. In the latter case this is given to you as enclosed content of the shortcode and you can fully modify it before placing it anywhere in your site.</p>
+
+<h3>Custom Shortcodes</h3>
+
+<p>This is an easy way to create predefined pieces of content which you can save and use anywhere. Editing the shortcode in the generator will automatically reflect all occurrences of this shortcode in your site.</p>
+
+<h2>Overriding Shortcode Revolution Templates</h2>
