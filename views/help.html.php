3364b3f4c72dde69426706460bca734f4243202b
pimteam <admin@pimteam.net>
2021-11-29 16:25:35 +0200

    working on the help

--- views/help.html.php
+++ views/help.html.php
@@ -10,7 +10,7 @@ It's techically possible to create the shortcodes manually but it's much easier 
 <p>The <b>posts</b> shortcode lets you search posts by IDs, categories, types, tags or phrases and display them like:</p>
 
 <ul>	
-	<li><b>Default / regular</b> - the posts will be listed with thumbnail, excerpt, publication time, etc. This design (and almost all other Shortcode Revolution templates) can be overridden as explained in the section "Overriding Shortcode Revolution Templates" at the end of this guide.</li>
+	<li><b>Default / regular</b> - the posts will be listed with thumbnail, excerpt, publication time, etc. This design (and many of the other Shortcode Revolution templates) can be overridden as explained in the section "Overriding Shortcode Revolution Templates" at the end of this guide.</li>
 	<li><b>List</b> is a short unordered list with the post titles and links.</li>
 	<li><b>Carousel</b> creates a slider / carousel from the posts that respond to your criteria. We use our own custom slider code - no bloated ready sliders, so it's very light and simple.</li>
 </ul>
@@ -18,3 +18,28 @@ It's techically possible to create the shortcodes manually but it's much easier 
 <p>The <b>related posts</b> shortcode finds and displays which are related to the current or selected post. You can give relation criteria like matching tags and/or categories. The display formats are the same as with the posts shortcode. This excellent shortcode can be added at the bottom of your single post template to always display related content without using yet another plugin.</p>
 
 <p>The <b>comments</b> shortcode displays comments related to a post or just most recent / best comments. The display formats are default and carousel / slider.</p>
+
+<h2>Popups / Modals</h2>
+
+<p>This shortcode lets you create modal windows / lightboxes with any predefined content. It generates a button to click on which appears on the place of the shortcode so you can use it anywhere in your site. This makes it super easy to create lightboxes without writing code or using more plugins.</p>
+<p>The modals can contain any kind of images and media, text, other shortcodes, and so on. If your modal contains anything other than plain unformtatted text make sure to insert it in Text mode of your rich text editor (when pasting it in posts, pages, etc).</p>
+
+<h2>Columns &amp; Grids</h2>
+
+<p>These shortcodes let you create modern cross-browser compatible and responsive content that flows in columns or a grid.</p>
+
+<p>The <b>newspaper-like option</b> creates content that automatically flows in multiple columns.</p>
+
+<p>The <b>grid option</b> is more precise in the way that your content flows on the page. You choose exactly what goes into each cell of the grid - the content does not low through the cells / columns. The grid option can also use your custom template.</p> 
+
+<h2>Tabs</h2>
+
+<p>The tabs shortcode lets you create tabbed content where the tabs do not reload the whole page. Very easy, light and responsive.</p>
+
+<h2>Buttons</h2>
+
+<p>With this shortcode you can create your own design for buttons - colors, size, style, fonts, and so on. Currently buttons support only URL action (going to another page). Calling custom JS functions is coming soon.</p>
+
+<h2>Tables</h2>
+
+<p>The tables shortcode gives you a super easy way to turn an Excel (CSV) sheet into a HTML table. The data is loaded on the fly so you don't need to recreate the shortcode when the file contents change.</p>
